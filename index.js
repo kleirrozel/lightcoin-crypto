dce58e2f36991883aecf748d39f895a4c741129a
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-02 16:14:55 -0400

    Finished the class oop LHL Lightcoin exercise

--- index.js
+++ index.js
@@ -1,29 +1,87 @@
 let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance () {
+    let balance = 0;
+    for (let t of this.transactions)  {
+      balance += t.value;
+    }
+    return balance;
   }
 
+  addTransaction(transaction) {
+  	this.transactions.push(transaction);
+  }
 }
 
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+
+  commit() {
+    if (!this.isAllowed())
+    return false;
+
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+}
+
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+myAccount = new Account("Elvis Bun");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Starting Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+const t1 = new Deposit(120.00, myAccount);
+console.log(t1.commit());
+
+const t2 = new Withdrawal(50.00, myAccount);
+console.log(t2.commit());
+
+const t3 = new Withdrawal(80.00, myAccount);
+console.log(t3.commit()); // EXPERIMENT: SHOULD NOT PRINT bc false (80 is more than what you have left in the account)
+
+const t4 = new Deposit(40.00, myAccount);
+console.log(t4.commit());
+
+console.log('Ending Balance: ', myAccount.balance);
+
+console.log('Account Transaction History: ', myAccount.transactions);
+
+
+// Circular reference  -- when I run it, transactions: [Circular *1] -- don't worry about it
